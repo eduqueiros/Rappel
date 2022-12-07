b87eaad31227895b90fdee99602f5e3c56fea36f
eduqueiros <eduardoqueiros10@gmail.com>
2022-12-07 14:03:39 +0000

    pagina perfil3

--- app/src/main/java/ipvc/estg/myapplication/perfil.kt
+++ app/src/main/java/ipvc/estg/myapplication/perfil.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.Toast
 
 class perfil : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater =menuInflater
         inflater.inflate(R.menu.menu, menu)
