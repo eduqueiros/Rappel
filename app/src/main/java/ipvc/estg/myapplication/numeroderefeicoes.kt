2a557f06c12133529574390b96334c0bb1e1266f
nunoleitao <npleitaoo@gmail.com>
2022-12-13 10:53:49 +0000

    Nunologin

---
+++ app/src/main/java/ipvc/estg/myapplication/numeroderefeicoes.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class numeroderefeicoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_numeroderefeicoes)
+    }
+}
