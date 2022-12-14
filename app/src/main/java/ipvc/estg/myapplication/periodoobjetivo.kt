a0b8a62e98d7b6b646f631d9f0c45975cc0da8fb
pedrorodrigues <pedro.rodrigues@ipvc.pt>
2022-12-14 16:58:14 +0000

    yes

--- app/src/main/java/ipvc/estg/myapplication/periodoobjetivo.kt
+++ app/src/main/java/ipvc/estg/myapplication/periodoobjetivo.kt
@@ -1,11 +1,17 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class periodoobjetivo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_periodoobjetivo)
+    }
+    fun objetivodieta(view: View){
+        val objetivodieta = Intent(this, objetivodieta::class.java)
+        startActivity(objetivodieta)
     }
 }
