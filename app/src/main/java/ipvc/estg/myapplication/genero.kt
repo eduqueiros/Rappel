741d7d2f360be27737d5beda45f1e37fb406da6b
pedrorodrigues <pedro.rodrigues@ipvc.pt>
2022-12-14 16:51:55 +0000

    yes

--- app/src/main/java/ipvc/estg/myapplication/genero.kt
+++ app/src/main/java/ipvc/estg/myapplication/genero.kt
@@ -1,11 +1,17 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class genero : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.genero)
+    }
+    fun nascimento(view: View){
+        val idade = Intent(this, idade::class.java)
+        startActivity(idade)
     }
 }
