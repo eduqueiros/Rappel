a88991ad828cf3b66ba8726737b19c77e6b7bbb3
eduqueiros <eduardoqueiros10@gmail.com>
2022-12-14 17:58:49 +0000

    login google

--- app/src/main/java/ipvc/estg/myapplication/dietas.kt
+++ app/src/main/java/ipvc/estg/myapplication/dietas.kt
@@ -1,11 +1,17 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class dietas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dietas)
+    }
+    fun perfil2(view: View) {
+        val perfil = Intent(this, perfil::class.java)
+        startActivity(perfil)
     }
 }
