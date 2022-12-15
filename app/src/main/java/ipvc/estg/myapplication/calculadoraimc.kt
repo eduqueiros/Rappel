f0cb63008db6c432b58e8ea86d4dfa1392e8d3e8
macbookpro <pedroreis@ipvc.pt>
2022-12-15 14:10:30 +0000

    mimc

--- app/src/main/java/ipvc/estg/myapplication/calculadoraimc.kt
+++ app/src/main/java/ipvc/estg/myapplication/calculadoraimc.kt
@@ -2,10 +2,85 @@ package ipvc.estg.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class calculadoraimc : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calculadoraimc)
+
+
+        supportActionBar!!.title = "Calculadora IMC"
+
+        calculate_btn.setOnClickListener {
+
+            // Check if the height EditText and Weight EditText are not empty
+            if (etHeight.text.isNotEmpty() && etWeight.text.isNotEmpty()) {
+                val height = (etHeight.text.toString()).toInt()
+                val weight = (etWeight.text.toString()).toInt()
+
+                // calculateBMI will return BMI
+                val IMC = calculateBMI(height, weight)
+
+                imc.text = IMC.toString()
+                imc.visibility = View.VISIBLE
+
+                // update the status text as per the bmi conditions
+                if (IMC < 18.5) {
+                    status.text = "Magro"
+                } else if (IMC >= 18.5 && IMC < 24.9) {
+                    status.text = "Saudavel"
+                } else if (IMC >= 24.9 && IMC < 30) {
+                    status.text = "Obeso"
+                } else if (IMC >= 30) {
+                    status.text = "Obesidade morbida"
+                }
+
+                imc_tv.visibility = View.VISIBLE
+                status.visibility = View.VISIBLE
+
+                ReCalculate.visibility = View.VISIBLE
+                calculate_btn.visibility = View.GONE
+
+            }
+
+            // when either Weight EditText or
+            // height EditText have null value
+            // we will display toast.
+            else {
+                Toast.makeText(this, "please enter the valid height and weight", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        ReCalculate.setOnClickListener {
+            ResetEverything()
+        }
+
     }
+
+    // Function to reset all Text and EditText fields.
+    private fun ResetEverything() {
+
+        calculate_btn.visibility = View.VISIBLE
+        ReCalculate.visibility = View.GONE
+
+        etHeight.text.clear()
+        etWeight.text.clear()
+        status.text = " "
+        imc.text = " "
+        imc_tv.visibility = View.GONE
+    }
+
+    // Function for calculating BMI
+    private fun calculateBMI(height: Int, weight: Int): Float {
+
+        val Height_in_metre = height.toFloat() / 100
+        val IMC = weight.toFloat() / (Height_in_metre * Height_in_metre)
+
+        return IMC }
+
+
+
+}
 }
