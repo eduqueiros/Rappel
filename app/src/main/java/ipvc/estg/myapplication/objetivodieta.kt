015d0f623be15fca578264efa2afe0a18687e4fe
pedrorodrigues <pedro.rodrigues@ipvc.pt>
2022-12-15 23:57:08 +0000

    yes

--- app/src/main/java/ipvc/estg/myapplication/objetivodieta.kt
+++ app/src/main/java/ipvc/estg/myapplication/objetivodieta.kt
@@ -1,11 +1,17 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class objetivodieta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.objetivodieta)
+    }
+    fun frente(view: View){
+        val pagina_inicial = Intent(this, pagina_inicial::class.java)
+        startActivity(pagina_inicial)
     }
 }
