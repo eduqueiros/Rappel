0924062aec0aa0ca42f67236963c15a8f5863142
RauulM <raulmoreira@ipvc.pt>
2022-12-15 16:30:21 +0000

    pagina receitas/receitas engordar

--- app/src/main/java/ipvc/estg/myapplication/Adapter.kt
+++ app/src/main/java/ipvc/estg/myapplication/Adapter.kt
@@ -1,7 +1,6 @@
 package ipvc.estg.myapplication
 
 import android.app.Activity
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +8,13 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 
-class Adapter (private val context : Activity, private val arrayList : ArrayList<Receita>) : ArrayAdapter<Receita>(context, R.layout.receitas_item,
+class Adapter (private val context : Activity, private val arrayList : ArrayList<Receita>) : ArrayAdapter<Receita>(context, R.layout.receitas_engordar,
     arrayList) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View{
 
         val inflater : LayoutInflater = LayoutInflater.from(context)
-        val view : View = inflater.inflate(R.layout.receitas_item,null)
+        val view : View = inflater.inflate(R.layout.receitas_engordar,null)
 
 
         val imageView : ImageView = view.findViewById(R.id.receitasImagem)
