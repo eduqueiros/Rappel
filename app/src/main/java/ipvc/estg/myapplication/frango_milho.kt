d452aabde1a9938ce0c91f2c1a365776ca221003
eduqueiros <eduardoqueiros10@gmail.com>
2023-01-12 15:03:54 +0000

    receitas

--- app/src/main/java/ipvc/estg/myapplication/frango_milho.kt
+++ app/src/main/java/ipvc/estg/myapplication/frango_milho.kt
@@ -1,11 +1,18 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class frango_milho : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_frango_milho)
+    }
+
+    fun voltar3(view: View){
+        val receitas_manter = Intent(this, receitas_manter::class.java)
+        startActivity(receitas_manter)
     }
 }
