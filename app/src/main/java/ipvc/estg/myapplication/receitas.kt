109e1a1388087aacec471816da634d3088d45778
eduqueiros <eduardoqueiros10@gmail.com>
2023-01-12 13:41:07 +0000

    bd2

---
+++ app/src/main/java/ipvc/estg/myapplication/receitas.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class receitas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receitas2)
+    }
+}
