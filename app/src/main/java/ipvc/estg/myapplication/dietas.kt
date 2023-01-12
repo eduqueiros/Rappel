2934efad9b921ca4f8bc2987c0449a1385c2d127
eduqueiros <eduardoqueiros10@gmail.com>
2023-01-12 23:13:11 +0000

    receitas

--- app/src/main/java/ipvc/estg/myapplication/dietas.kt
+++ app/src/main/java/ipvc/estg/myapplication/dietas.kt
@@ -4,11 +4,67 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import ipvc.estg.myapplication.databinding.ActivityDietasBinding
 
 class dietas : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDietasBinding
+    private lateinit var database : DatabaseReference
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dietas)
+        binding = ActivityDietasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.readdataBtn.setOnClickListener {
+
+            val Nome : String = binding.etusername.text.toString()
+            if  (Nome.isNotEmpty()){
+
+                readData(Nome)
+
+            }else{
+
+                Toast.makeText(this,"PLease enter the Name of the product", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
+
+    }
+
+    private fun readData(Nome: String) {
+
+        database = FirebaseDatabase.getInstance().getReference("Produtos")
+        database.child(Nome).get().addOnSuccessListener {
+
+            if (it.exists()){
+
+                val nome = it.child("Nome").value
+                val descricao = it.child("Descrição").value
+                Toast.makeText(this,"Successfuly Read", Toast.LENGTH_SHORT).show()
+                binding.etusername.text.clear()
+                binding.tvFirstName.text = nome.toString()
+                binding.tvLastName.text = descricao.toString()
+
+            }else{
+
+                Toast.makeText(this,"Product Doesn't Exist", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+        }.addOnFailureListener{
+
+            Toast.makeText(this,"Failed", Toast.LENGTH_SHORT).show()
+
+
+        }
+
     }
     fun perfil2(view: View) {
         val perfil = Intent(this, perfil::class.java)
