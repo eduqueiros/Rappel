e5214a9ef76d865a1cb604f7b67f15f7f674b3fa
eduqueiros <eduardoqueiros10@gmail.com>
2023-01-12 14:13:16 +0000

    receitas

---
+++ app/src/main/java/ipvc/estg/myapplication/omelete_espinafre.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class omelete_espinafre : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_omelete_espinafre)
+    }
+}
--- app/src/main/java/ipvc/estg/myapplication/receitas_manter.kt
+++ app/src/main/java/ipvc/estg/myapplication/receitas_manter.kt
@@ -1,11 +1,18 @@
 package ipvc.estg.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class receitas_manter : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receitas_manter)
+    }
+
+    fun espinafre(view: View){
+        val omelete_espinafre = Intent(this, omelete_espinafre::class.java)
+        startActivity(omelete_espinafre)
     }
 }
